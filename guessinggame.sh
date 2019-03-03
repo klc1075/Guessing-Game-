8a728c92e347ed25d2d2189ee79e9f823f61fa44
klc1075 <48163085+klc1075@users.noreply.github.com>
2019-03-03 18:33:06 -0500

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,22 @@
+choice=-1
+correct=7
+typeset -i num=0
 
+
+echo "How many files are in the working directory?"
+
+###Answer
+(( answer = 7))
+
+while (( choice !=answer )); do
+        num=num+1
+        read -p "Enter choice $num: " choice
+        if (( choice < correct )); then
+                echo "Too Low"
+                echo "Try again"
+        elif (( choice > correct )); then
+                echo "Too High"
+                echo "Try again"
+        fi
+done
+echo "Winner Winner Chicken Dinner"
